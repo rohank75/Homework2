83282a018db93b5dc3bdd8e08239faba6f1441b6
rohank75 <94821292+rohank75@users.noreply.github.com>
2022-01-07 18:03:10 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // write your code here
+
         System.out.println("Hello, World!");
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("What is your name?: ");
+        String name = s.next();
+
+        System.out.print("What is your age?: ");
+        int age = s.nextInt();
+
+        System.out.print("What is your weight?: ");
+        Double weight = s.nextDouble();
+
+        System.out.println("Name: " + name + ", " + "Age: " + age + ", " + "Weight: " + weight);
+
     }
 }
